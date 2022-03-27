3ccd7ba4e373740d15f41ba7c0073c18940d9226
ViniciusAlves-Hacker <dev.viniciusalves@gmail.com>
2022-03-27 19:05:13 -0300

    Demonstrando eventos em admin.html e incluindo admin.js

---
+++ js/admin.js
@@ -0,0 +1,32 @@
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+
+const trAtual = document.getElementById("lista-eventos-gerenciamento");
+
+window.onload = async () => {
+    const resposta = await fetch(`${BASE_URL}/events`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const response = await resposta.json()
+    console.log(response);
+
+
+    response.forEach(item => {
+        newDate = `${item.scheduled[8]}${item.scheduled[9]}/${item.scheduled[5]}${item.scheduled[6]}/${item.scheduled[0]}${item.scheduled[1]}${item.scheduled[2]}${item.scheduled[3]}  ${item.scheduled[11]}${item.scheduled[12]}:${item.scheduled[14]}${item.scheduled[15]}`
+        trAtual.innerHTML += `<tr>
+        <th scope="row">1</th>
+        <td>${newDate}</td>
+        <td>${item.name}</td>
+        <td>${item.attractions}</td>
+        <td>
+            <a href="reservas.html" class="btn btn-dark">ver reservas</a>
+            <a href="editar.html" class="btn btn-secondary">editar</a>
+            <a href="excluir-evento.html" class="btn btn-danger">excluir</a>
+        </td>
+    </tr>`
+    })
+
+}
